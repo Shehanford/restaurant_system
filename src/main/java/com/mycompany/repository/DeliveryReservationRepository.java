29ccb7b68276951662eb28607ff5fad2a8dacd2e
Shehanford <shehanrford@gmail.com>
2024-09-10 18:12:14 +0530

    Repository update

--- src/main/java/com/mycompany/repository/DeliveryReservationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mycompany.repository;
-
-import com.mycompany.model.DeliveryReservation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DeliveryReservationRepository extends JpaRepository<DeliveryReservation, Integer> {
-}
--- src/main/java/com/mycompany/repository/UserRepository.java
+++ src/main/java/com/mycompany/repository/UserRepository.java
@@ -2,7 +2,6 @@ package com.mycompany.repository;
 
 import com.mycompany.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends JpaRepository<User,Integer> {
 
